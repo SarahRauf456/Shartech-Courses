5d32832dabe966e4f0459855c279746e602d53fa
Sharfia <s86211368@gmail.com>
2025-12-26 18:11:17 +0530

    Fix formatting in script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ window.addEventListener("scroll", () => {
 function openModal(id) {
   document.getElementById(id).style.display = "flex";
 }
-
 function closeModal(id) {
   document.getElementById(id).style.display = "none";
 }
